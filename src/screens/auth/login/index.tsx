1bfba041fabfdc634c9bb8dda9393bba4ac870ca
asimmehmood.codingpixel11 <asim.mehmood@codingpixel.com>
2022-09-09 01:44:44 +0500

    implemented gmail login

--- src/screens/auth/login/index.tsx
+++ src/screens/auth/login/index.tsx
@@ -1,37 +1,94 @@
-//import liraries
-import React, {Component} from 'react';
-import {View, Text, StyleSheet, Pressable} from 'react-native';
+import React from 'react';
+import {View, StyleSheet, Platform, Text} from 'react-native';
+import colors from '../../../assets/colors/colors';
+import images from '../../../assets/images/images';
+import Button from '../../../shared/components/button/button';
 import Wrapper from '../../../shared/components/wrapper';
+import {RF} from '../../../shared/theme/responsive';
+import FirebaseHelper from '../../../shared/utils/FirebaseHelper';
 
-// create a component
 const Login = () => {
+  const googleSignIn = () => {
+    FirebaseHelper.googleSignIn((user: any) => {
+    });
+  };
   return (
     <Wrapper>
       <View style={styles.container}>
-        <Pressable
-          style={{backgroundColor: 'green', padding: 12, marginBottom: 15}}
-          onPress={() => {}}>
-          <Text style={{color: '#fff'}}>Login with Google</Text>
-        </Pressable>
+        <Text style={styles.heading}>Social Login</Text>
 
-        <Pressable
-          style={{backgroundColor: 'blue', padding: 12}}
-          onPress={() => {}}>
-          <Text style={{color: '#fff'}}>Login with Facebook</Text>
-        </Pressable>
+        <View style={{marginVertical: RF(5)}}>
+          <Button
+            title={'Continue with Google'}
+            bgColor={'transparent'}
+            iconPlace={'leftCenter'}
+            titleStyle={{
+              color: colors.BLACK,
+              fontSize: RF(13),
+            }}
+            isIcon={true}
+            icon={images.google}
+            marginRightIcon={RF(4)}
+            width={'90%'}
+            onPress={() => {
+              googleSignIn();
+            }}
+          />
+        </View>
+        <View style={{marginVertical: RF(5)}}>
+          <Button
+            title={'Continue with Facebook'}
+            bgColor={colors.FACEBOOK}
+            iconPlace={'leftCenter'}
+            titleStyle={{
+              color: colors.WHITE,
+              fontSize: RF(13),
+            }}
+            isIcon={true}
+            iconTintColor={colors.WHITE}
+            marginRightIcon={RF(4)}
+            icon={images.facebook}
+            width={'90%'}
+            onPress={() => {}}
+          />
+        </View>
+
+        {Platform.OS === 'ios' && (
+          <View style={{marginVertical: RF(5)}}>
+            <Button
+              title={'Continue with Apple'}
+              bgColor={colors.BLACK}
+              iconPlace={'leftCenter'}
+              titleStyle={{
+                color: colors.WHITE,
+                fontSize: RF(13),
+              }}
+              isIcon={true}
+              iconTintColor={colors.WHITE}
+              marginRightIcon={RF(4)}
+              icon={images.apple}
+              width={'90%'}
+              onPress={() => {}}
+            />
+          </View>
+        )}
       </View>
     </Wrapper>
   );
 };
 
-// define your styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  heading: {
+    fontWeight: '600',
+    fontSize: RF(22),
+    marginBottom: RF(10),
   },
 });
 
-//make this component available to the app
 export default Login;
---
+++ src/shared/utils/FirebaseHelper.js
@@ -0,0 +1,90 @@
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+// import {Settings, Profile} from 'react-native-fbsdk-next';
+// import {LoginButton, AccessToken, LoginManager} from 'react-native-fbsdk-next';
+// import { appleAuth } from '@invertase/react-native-apple-authentication';
+import auth from '@react-native-firebase/auth';
+
+class firebaseHelper {
+  constructor() {
+    this.googleCofiguration();
+    // Settings.initializeSDK();
+  }
+
+  googleCofiguration() {
+    GoogleSignin.configure({
+      scopes: ['https://www.googleapis.com/auth/drive.readonly'],
+      webClientId:
+        '102991734320-hodp5ogmqo2atvbk7s4t7n3792ddim7f.apps.googleusercontent.com',
+      offlineAccess: true,
+    });
+  }
+  googleSignIn = async callback => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const {user, idToken} = await GoogleSignin.signIn();
+
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+      let googleAuth = auth().signInWithCredential(googleCredential);
+      callback(user, googleCredential, googleAuth);
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+      } else {
+      }
+    }
+  };
+
+  //   facebookSignIn = async callback => {
+  //     await LoginManager.logOut();
+  //     const result = await LoginManager.logInWithPermissions([
+  //       // 'public_profile',
+  //       // 'email', "user_friends"
+  //       'public_profile',
+  //       'email',
+  //       // 'user_friends'
+  //     ]);
+  //     if (result.isCancelled) {
+  //       throw 'User cancelled the login process';
+  //     }
+  //     const data = await AccessToken.getCurrentAccessToken();
+  //     if (!data) {
+  //       throw 'Something went wrong obtaining access token';
+  //     }
+
+  //     const facebookCredential = auth.FacebookAuthProvider.credential(
+  //       data.accessToken,
+  //     );
+  //     auth()
+  //       .signInWithCredential(facebookCredential)
+  //       .then(response => {
+  //         callback(response?.user);
+  //       });
+
+  //     //  Profile.getCurrentProfile(data.accessToken).then((res)=>{
+  //     //      console.log("prfile Data ====>>>",res);
+  //     //
+  //     //      callback(res);
+  //     // })
+  //   };
+
+  //  appleSignIn  = async(callback)=> {
+  //   const appleAuthRequestResponse = await appleAuth.performRequest({
+  //     requestedOperation: appleAuth.Operation.LOGIN,
+  //     requestedScopes: [appleAuth.Scope.EMAIL, appleAuth.Scope.FULL_NAME],
+  //   });
+  //   if (!appleAuthRequestResponse.identityToken) {
+  //     throw new Error('Apple Sign-In failed - no identify token returned');
+  //   }
+  //   const { identityToken, nonce } = appleAuthRequestResponse;
+  //   const appleCredential = auth.AppleAuthProvider.credential(identityToken, nonce);
+  //   const data =  auth().signInWithCredential(appleCredential);
+  //   callback(data);
+  // }
+}
+const FirebaseHelper = new firebaseHelper();
+export default FirebaseHelper;
